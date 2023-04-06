99ec7a97c277e7672f184dc3e050bcacbb8df546
wolegechu <awolegechu@gmail.com>
2023-04-07 07:00:53 +0800

    update

--- prompt/task-interpreter/task-interpreter.py
+++ prompt/task-interpreter/task-interpreter.py
@@ -2,7 +2,16 @@ import promptlayer
 import os
 import json
 import jinja2
+from langchain.llms import OpenAI, Anthropic
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.schema import HumanMessage
 
+llm = OpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
+resp = llm("Write me a song about sparkling water.")
+
+exit()
 promptlayer.api_key = os.environ['PROMPT_API_KEY']
 
 # Swap out your 'import openai'
@@ -32,10 +41,7 @@ example_input = json.load(f)
 """
 
 user_content = '''
-Please provide a step-by-step plan that satisfies the following user requirement: "{{ user_requirement }}". Consider the provided protocol descriptions and choose the most suitable protocols and subcontracts to use in each step. Include the necessary ERC20/ERC721 token information if needed.
-
 The output schema must be:
-
 - steps: list
     - step_number
     - action
@@ -47,30 +53,39 @@ The output schema must be:
             - name
             - address
             - description
-
-
 Protocol Descriptions:
-{for protocol in protocols}
-- {protocol.name}: {protocol.description}. Supported chains: {', '.join(protocol.supported_chains)}. Subcontracts: {', '.join([subcontract.name for subcontract in protocol.subcontracts])}.
-{end}
+{% for protocol in protocols -%}
+- {{ protocol.name }}: {{ protocol.description }}. 
+    Supported chains: {{ ', '.join(protocol.supported_chains) }}. 
+    Subcontracts:
+    {% for subcontract in protocol.subcontracts -%}
+        - {{ subcontract.name }}: {{ subcontract.description }}
+        {% for chain in subcontract.chains -%}
+        - {{ chain.name }}: {{ chain.address }}
+        {% endfor -%}
+    {% endfor -%}
+{% endfor -%}
 
 ERC20/ERC721 Tokens:
-{for token in tokens}
-- {token.type}: {token.name} ({token.symbol}), Address: {token.address}, Chain: {token.chain}.
-{end}
+{% for token in tokens -%}
+- {{ token.type }}: {{ token.name }} ({{ token.symbol }})
+    {% for chain in token.chains -%}
+    - Address: {{chain.address}}, Chain: {{chain.name}}
+    {% endfor -%}
+{% endfor -%}
 '''
 
 template = jinja2.Template(user_content)
-output = template.render(example_input)
-print(output)
+output = template.render(example_input, trim_blocks=True,
+                         lstrip_blocks=True, keep_trailing_newline=True, autoescape=False)
 
-exit()
+user_requirement = example_input['user_requirement']
 openai.ChatCompletion.create(
     model="code-davinci-002",
     messages=[
-        {"role": "system", "content": "Create a summary for generating an executable program using the information. Ensure the output includes contract addresses, method signatures, and any necessary additional checks. If any additional checks are needed before performing the actions, include them in the summary."},
-        {"role": "user", "content": user_content},
+        {"role": "system", "content": f'Please provide a step-by-step plan that satisfies the following user requirement "{user_requirement}". Consider the provided protocol descriptions and choose the most suitable protocols and subcontracts to use in each step. Include the necessary ERC20/ERC721 token information if needed.'},
+        {"role": "user", "content": output},
 
     ],
-    pl_tags=["program"]
+    pl_tags=["keyword"]
 )
