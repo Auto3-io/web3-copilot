46d8b990a682bed5e36a9cf7cf2a9d57e9638781
wolegechu <awolegechu@gmail.com>
2023-04-08 06:25:22 +0800

    feat: add method search engine

--- copilot/method_search_engine/engine.py
+++ copilot/method_search_engine/engine.py
@@ -2,20 +2,12 @@ from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredicto
 from langchain.chat_models import ChatOpenAI
 
 def search_engine(steps: list[str]):
-    results = []
-    for step in steps:
-        documents = SimpleDirectoryReader('data').load_data()
-        llm_predictor = LLMPredictor(llm=ChatOpenAI(
-            temperature=0, model_name="gpt-3.5-turbo"))
-        service_context = ServiceContext.from_defaults(
-            llm_predictor=llm_predictor, chunk_size_limit=512)
+    index = GPTSimpleVectorIndex.load_from_disk('./index')
 
-        index = GPTSimpleVectorIndex.from_documents(
-            documents, service_context=service_context)
-        response = index.query(
-            "What did the author do growing up?",
-            service_context=service_context,
-            similarity_top_k=3
-        )
-        print(response)
-        results.append(response)
+    response = index.query(
+        "Swap 1000 USDC for ETH on Uniswap.",
+        service_context=service_context,
+        similarity_top_k=10
+    )
+
+    print(response)
